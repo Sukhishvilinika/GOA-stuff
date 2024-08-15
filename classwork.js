63167fb14d4a4b1a8e01d90fad042e98a7088c0a
Sukhishvilinika <thyzestytin@gmail.com>
2024-08-15 16:50:11 +0400

    upload

---
+++ classwork.js
@@ -0,0 +1,4 @@
+let number = 300; // აქ იქმნება ცვლადი და მიეცემა მნიშვნელობა
+let people = "aragveli";
+let metro = number + people;
+console.log(metro) // აქ გამოდის მოყვანილი ცვლადის მნიშვნელობას
